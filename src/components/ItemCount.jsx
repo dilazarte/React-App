0893cf8113f4e79b272f8fe12a54d4032d68de32
David Lazarte <dilazarte@gmail.com>
2022-01-28 03:17:20 -0300

    Se envia por parametro la funcion onAdd al ItemCount para que al momento de dar click en agregar al carro se envie la cantidad y desaparezca el itemCount y muestre el boton de ir al carrito que redirecciona a /cart

--- src/components/ItemCount.jsx
+++ src/components/ItemCount.jsx
@@ -1,18 +1,20 @@
 import React, {useState, useEffect} from 'react'
+import { HiMinus, HiPlus } from "react-icons/hi";
 
 function ItemCount({stock, initial, onAdd}) {
     const [cantidad, setCantidad] = useState(initial)
+    
     return (
         <div className='itemCountContiner'>
             <div>
-                <button className='controlSust' disabled={cantidad === 1} onClick={()=> setCantidad(cantidad-1)}> - </button>
+                <button className='controlSust' disabled={cantidad === 1} onClick={()=> setCantidad(cantidad-1)}><HiMinus /></button>
                 <span className="cantidadNum"> {cantidad} </span>
-                <button className='controlAdd' disabled={stock=== cantidad} onClick={()=> setCantidad(cantidad+1)}> + </button>
+                <button className='controlAdd' disabled={stock=== cantidad} onClick={()=> setCantidad(cantidad+1)}><HiPlus /></button>
             </div>
-            <button className='addToCard' onClick={ ()=> {alert(`Se agregaron ${cantidad} productos al carrito`)
-            setCantidad(initial)} }>Agregar al carrito</button>
+            <button className='addToCart' onClick={ ()=> onAdd(cantidad) }>Agregar al carrito</button>
         </div>
     )
 }
 
 export default ItemCount
+// ()=> {alert(`Se agregaron ${cantidad} productos al carrito`) setCantidad(initial)}
