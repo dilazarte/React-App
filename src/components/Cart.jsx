ef8bb73b8ccac1c3ab0e50c917a615a0e70e667c
David Lazarte <dilazarte@gmail.com>
2022-02-03 02:45:31 -0300

    Se configura el componente Cart.jsx para que muestre los articulos agregados, cada Item cuenta con controles para eliminar, aumentar y disminuir la cantidad, aparte se agregó un metodo para calcular el precio total de los items agregados y un mensaje y boton para volver al inicio en caso de que esté vacio

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,11 +1,12 @@
 import React, {useContext, useEffect} from 'react';
+import { Link } from 'react-router-dom'
 import { cartContext } from './CartContext'
 import ItemCart from './ItemCart';
 
 function Cart() {
-    const {carrito, setCarrito, removeItem, clearCart} = useContext(cartContext)
+    const {carrito, setCarrito, removeItem, clearCart, totalPrice} = useContext(cartContext)
     return(
-        <>
+        <div className='cartProducts'>
         {
             (carrito.length > 0) ?
             <>
@@ -13,10 +14,15 @@ function Cart() {
                     carrito.map(item => <ItemCart key={item.id} item={item} />)
                 }
                 <button onClick={()=>clearCart()}>Vaciar Carrito!</button>
+                <h1>Total: {totalPrice.toFixed(2)}</h1>
             </>
-            : <h1>El carrito está vacio...</h1>
+            :
+            <div style={{display: 'flex', flexFlow: 'column', alignItems: 'center', marginTop: '5em'}}>
+                <h1>El carrito está vacio...</h1>
+                <Link to={'/'}><button>Volver al inicio</button></Link>
+            </div>
         }
-        </>
+        </div>
     )
 }
 
--- src/components/ItemCart.jsx
+++ src/components/ItemCart.jsx
@@ -3,13 +3,15 @@ import { cartContext } from './CartContext'
 import CartContext from './CartContext';
 
 function ItemCart({item}) {
-    const {carrito, setCarrito, removeItem} = useContext(cartContext)
+    const {carrito, setCarrito, removeItem, sumar, restar} = useContext(cartContext)
 
     return(
         <div className='cartContainer'>
-            <div style={{border: '1px solid red', padding: '5px', margin: '5px'}}>
+            <div style={{border: '1px solid red', padding: '5px', marginTop: '5px', marginBottom: '5px'}}>
                 <h5 key={item.id}> {item.title} - Cantidad: {item.quantity}</h5>
                 <button onClick={()=>removeItem(item.id)}>borrar!</button>
+                <button onClick={()=>sumar(item.id)}>+</button>
+                <button onClick={()=>restar(item.id)}>-</button>
             </div>
         </div>   
     )
